a9b14765fdd18b9aa2cd5ea5037dd990a587add9
Nevah5 <neevaah5@gmail.com>
2024-05-19 19:21:54 +0200

    Formatted and improved the code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const urlRegex = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/;
 
 const file = fs.readFileSync('input.txt').toString();
-const links = file.replace(/\r\n/g, '\n').split("\n");
+const links = file.replace(/\r\n/g, '\n').split("\n").filter(link => link.trim() !== "");
 
 var YoutubeMp3Downloader = require("youtube-mp3-downloader");
 var YD = new YoutubeMp3Downloader({
@@ -18,7 +18,7 @@ var YD = new YoutubeMp3Downloader({
 
 const downloadPromise = (ytID, index) => {
   return new Promise((resolve, reject) => {
-    YD.download(ytID, (index+1)+".mp3");
+    YD.download(ytID, (index + 1) + ".mp3");
     YD.on("finished", (err, data) => {
       resolve(data);
     });
@@ -28,18 +28,18 @@ const downloadPromise = (ytID, index) => {
 const finishedDownload = async (links) => {
   var downloads = 0;
   return new Promise(async (resolve, reject) => {
-    for (i = 0; i < links.length; i++){
-      if(urlRegex.test(links[i])){
+    for (i = 0; i < links.length; i++) {
+      if (urlRegex.test(links[i])) {
         const ytID = urlRegex.exec(links[i])[6];
         await downloadPromise(ytID, i).then(data => {
-          if(typeof data == "object"){
-            let fileName = `${i+1} - ${data.videoTitle}.mp3`;
-            fs.renameSync(`${process.env.OUTPUT_PATH}/${i+1}.mp3`, `${process.env.OUTPUT_PATH}/${fileName}`);
+          if (typeof data == "object") {
+            let fileName = `${i + 1} - ${data.videoTitle}.mp3`;
+            fs.renameSync(`${process.env.OUTPUT_PATH}/${i + 1}.mp3`, `${process.env.OUTPUT_PATH}/${fileName}`);
             log(`\✅ Finished Downloading "${data.videoTitle}"!`);
           }
         });
         downloads++;
-      }else{
+      } else {
         log(`\❌ "${links[i]}" is not a valid YouTube link!`);
       }
     }
@@ -51,6 +51,6 @@ const finishedDownload = async (links) => {
 log("Download of all videos started...\n");
 
 finishedDownload(links)
-.then((downloads) => {
-  log(`\n\nFinished downloading ${downloads} YouTube videos.`);
-});
+  .then((downloads) => {
+    log(`\n\nFinished downloading ${downloads} YouTube videos.`);
+  });
